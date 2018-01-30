4e10473313d5d646febb5120a07fde99d89c8707
Nicole Lin <nlin.nicole@gmail.com>
2018-01-30 18:54:28 -0500

    Started horse

--- a1_27890834/template/lab.cpp
+++ a1_27890834/template/lab.cpp
@@ -1,29 +1,33 @@
-// template.cpp : Defines the entry point for the console application.
-//
+//Lab 1
+//modified from http://learnopengl.com/
 
 #include "stdafx.h"
 
-#include <gtc/matrix_transform.hpp>
-#include <gtc/type_ptr.hpp>
+#include "..\glew\glew.h"	// include GL Extension Wrangler
+#include "..\glfw\glfw3.h"	// include GLFW helper library
+#include <stdio.h>
 #include <iostream>
+#include <string>
 #include <fstream>
 
+#include "glm.hpp"
+#include "gtc/matrix_transform.hpp"
+#include "gtc/type_ptr.hpp"
 
-const GLuint WINDOW_WIDTH = 800, WINDOW_HEIGHT = 600;
+using namespace std;
+
+// Window dimensions
+const GLuint WIDTH = 800;
+const GLuint HEIGHT = 800;
 GLFWwindow* window;
 
 GLuint loadShaders(std::string vertex_shader_path, std::string fragment_shader_path) {
-
-	// Create the shaders
-	GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
-	GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
-
 	// Read the Vertex Shader code from the file
-	std::string VertexShaderCode;
-	std::ifstream VertexShaderStream(vertex_shader_path, std::ios::in);
+	string VertexShaderCode;
+	std::ifstream VertexShaderStream(vertex_shader_path, ios::in);
 
 	if (VertexShaderStream.is_open()) {
-		std::string Line = "";
+		string Line = "";
 		while (getline(VertexShaderStream, Line))
 			VertexShaderCode += "\n" + Line;
 		VertexShaderStream.close();
@@ -31,7 +35,7 @@ GLuint loadShaders(std::string vertex_shader_path, std::string fragment_shader_p
 	else {
 		printf("Impossible to open %s. Are you in the right directory ?\n", vertex_shader_path.c_str());
 		getchar();
-		exit(-1);
+		return -1;
 	}
 
 	// Read the Fragment Shader code from the file
@@ -45,138 +49,290 @@ GLuint loadShaders(std::string vertex_shader_path, std::string fragment_shader_p
 		FragmentShaderStream.close();
 	}
 	else {
-		printf("Impossible to open %s. Are you in the right directory ?\n", fragment_shader_path.c_str());
+		printf("Impossible to open %s. Are you in the right directory?\n", fragment_shader_path.c_str());
 		getchar();
-		exit(-1);
+		return -1;
 	}
 
-	GLint Result = GL_FALSE;
-	int InfoLogLength;
-
-	// Compile Vertex Shader
-	printf("Compiling shader : %s\n", vertex_shader_path.c_str());
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	char const * VertexSourcePointer = VertexShaderCode.c_str();
-	glShaderSource(VertexShaderID, 1, &VertexSourcePointer, nullptr);
-	glCompileShader(VertexShaderID);
-
-	// Check Vertex Shader
-	glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
-	glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-	if (InfoLogLength > 0) {
-		std::vector<char> VertexShaderErrorMessage(InfoLogLength + 1);
-		glGetShaderInfoLog(VertexShaderID, InfoLogLength, nullptr, &VertexShaderErrorMessage[0]);
-		printf("%s\n", &VertexShaderErrorMessage[0]);
+	glShaderSource(vertexShader, 1, &VertexSourcePointer, NULL);
+	glCompileShader(vertexShader);
+	// Check for compile time errors
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+		return -1;
 	}
-
-	// Compile Fragment Shader
-	printf("Compiling shader : %s\n", fragment_shader_path.c_str());
+	// Fragment shader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	char const * FragmentSourcePointer = FragmentShaderCode.c_str();
-	glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer, nullptr);
-	glCompileShader(FragmentShaderID);
-
-	// Check Fragment Shader
-	glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
-	glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-	if (InfoLogLength > 0) {
-		std::vector<char> FragmentShaderErrorMessage(InfoLogLength + 1);
-		glGetShaderInfoLog(FragmentShaderID, InfoLogLength, nullptr, &FragmentShaderErrorMessage[0]);
-		printf("%s\n", &FragmentShaderErrorMessage[0]);
+	glShaderSource(fragmentShader, 1, &FragmentSourcePointer, NULL);
+	glCompileShader(fragmentShader);
+	// Check for compile time errors
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+		return -1;
 	}
+	// Link shaders
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
 
-	// Link the program
-	printf("Linking program\n");
-	GLuint ProgramID = glCreateProgram();
-	glAttachShader(ProgramID, VertexShaderID);
-	glAttachShader(ProgramID, FragmentShaderID);
-
-	glBindAttribLocation(ProgramID, 0, "in_Position");
-
-	glLinkProgram(ProgramID);
-
-	// Check the program
-	glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
-	glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-	if (InfoLogLength > 0) {
-		std::vector<char> ProgramErrorMessage(InfoLogLength + 1);
-		glGetProgramInfoLog(ProgramID, InfoLogLength, nullptr, &ProgramErrorMessage[0]);
-		printf("%s\n", &ProgramErrorMessage[0]);
+	// Check for linking errors
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+		return -1;
 	}
+	glDeleteShader(vertexShader); //free up memory
+	glDeleteShader(fragmentShader);
 
-	glDeleteShader(VertexShaderID); //free up memory
-	glDeleteShader(FragmentShaderID);
+	return shaderProgram;
+}
 
-	return ProgramID;
+// Is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	std::cout << key << std::endl;
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
 int init() {
 	std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
-	// Init GLFW
 	glfwInit();
-	// Set all the required options for GLFW
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
 	// Create a GLFWwindow object that we can use for GLFW's functions
-	window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Lab", nullptr, nullptr);
+	window = glfwCreateWindow(WIDTH, HEIGHT, "Horse", nullptr, nullptr);
 	if (window == nullptr)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
+
+	glfwSetKeyCallback(window, key_callback);
 	glfwMakeContextCurrent(window);
-	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
 	glewExperimental = GL_TRUE;
-	// Initialize GLEW to setup the OpenGL Function pointers
-	if (glewInit() != GLEW_OK)
-	{
+
+	if (glewInit() != 0) {
 		std::cout << "Failed to initialize GLEW" << std::endl;
 		return -1;
 	}
 
+	return 0;
+}
+
+// The MAIN function, from here we start the application and run the game loop
+int main()
+{
+	if (init() != 0) {
+		std::cout << "Failed to initialize program" << std::endl;
+		return -1;
+	}
 	// Define the viewport dimensions
 	int width, height;
 	glfwGetFramebufferSize(window, &width, &height);
 
 	glViewport(0, 0, width, height);
 
-	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LESS);
-}
-
-int main()
-{
-	if (init() < 0) {
-		return -1;
-	}
-	
 	GLuint shaderProgram = loadShaders("vertex.shader", "fragment.shader");
 	glUseProgram(shaderProgram);
 
-	std::vector<glm::vec3> vertices = { glm::vec3(-1,0,0),glm::vec3(0,1,0),glm::vec3(1,0,0) };
-
-	GLuint VBO, VAO;
+	GLfloat vertices[] = {
+		1.0f,-1.0f, 1.0f, //Cube begin
+		-1.0f,-1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f //Cube end
+		//100.0f, 0.0f, 100.0f, //Ground begin
+		//-100.0f, 0.0f, 100.0f,
+		//-100.0f, 0.0f, -100.0f,
+		//100.0f, 0.0f, -100.0f //Ground end
+	};
+	GLuint VAO;
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
 
+	GLuint VBO;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3)*vertices.size(), &vertices[0], GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0); 
+	glBindVertexArray(0);
 
+	//Model Matrix
+	glm::mat4 scale;
+	glm::mat4 translate;
+	glm::mat4 rotate;
+	GLuint transformLoc = glGetUniformLocation(shaderProgram, "model_matrix");
 
-	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	//View Matrix
+	glm::mat4 viewMatrix = glm::lookAt(glm::vec3(0.0f, 0.0f, 15.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	
+	GLuint viewMatrixLoc = glGetUniformLocation(shaderProgram, "view_matrix");
+
+	//Projection Matrix
+	glm::mat4 projectionMatrix = glm::perspective(45.0f, (GLfloat)width / (GLfloat)height, 0.0f, 100.0f);
+	GLuint projectionLoc = glGetUniformLocation(shaderProgram, "projection_matrix");
+
+	glUniformMatrix4fv(viewMatrixLoc, 1, GL_FALSE, glm::value_ptr(viewMatrix));
+	glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projectionMatrix));
+	glClearColor(0.7f, 0.7f, 0.7f, 0.0f);
+
+	// Game loop
 	while (!glfwWindowShouldClose(window))
 	{
-
 		glfwPollEvents();
-		glfwSwapBuffers(window);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glDrawArrays(GL_TRIANGLES, 0, vertices.size());
 
+		// Render
+		// Clear the colorbuffer
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//GROUND
+		/*glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 4);
+		glBindVertexArray(0);
+		glm::mat4 ground;
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(ground));*/
+
+
+		//HEAD
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 12*3);
+		glBindVertexArray(0);
+
+		glm::mat4 head;
+		rotate = glm::rotate(head, 45.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+		scale = glm::scale(head, glm::vec3(1.0f, 0.5f, 1.0f));
+		translate = glm::translate(head, glm::vec3(-5.0f, 5.0f, 0.0f));
+		head = head * rotate * translate * scale;
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(head));
+
+		//NECK
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 12 * 3);
+		glBindVertexArray(0);
+
+		glm::mat4 neck;
+		rotate = glm::rotate(neck, 20.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+		scale = glm::scale(neck, glm::vec3(0.6f, 1.0f, 1.0f));
+		translate = glm::translate(neck, glm::vec3(0.0f, 2.5f, 0.0f));
+		neck = neck * rotate * scale * translate;
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(neck));
+
+		//TORSO
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 12 * 3);
+		glBindVertexArray(0);
+
+		glm::mat4 torso;
+		scale = glm::scale(torso, glm::vec3(2.0f, 1.0f, 1.0f));
+		torso *= scale;
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(torso));
+
+		//UPPER ARM L
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 12 * 3);
+		glBindVertexArray(0);
+
+		glm::mat4 upperArmL;
+		scale = glm::scale(upperArmL, glm::vec3(0.3f, 0.7f, 0.3f));
+		translate = glm::translate(upperArmL, glm::vec3(-6.0f, -2.5f, 0.0f));
+		upperArmL *= scale * translate;
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(upperArmL));
+
+		//LOWER ARM L
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 12 * 3);
+		glBindVertexArray(0);
+
+		glm::mat4 lowerArmL;
+		scale = glm::scale(lowerArmL, glm::vec3(0.3f, 0.7f, 0.3f));
+		translate = glm::translate(lowerArmL, glm::vec3(-6.0f, -4.5f, 0.0f));
+		lowerArmL *= scale * translate;
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(lowerArmL));
+
+		//UPPER LEG L
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 12 * 3);
+		glBindVertexArray(0);
+
+		glm::mat4 upperLegL;
+		scale = glm::scale(upperLegL, glm::vec3(0.3f, 0.7f, 0.3f));
+		translate = glm::translate(upperLegL, glm::vec3(6.0f, -2.5f, 0.0f));
+		upperLegL *= scale * translate;
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(upperLegL));
+
+		//LOWER LEG L
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 12 * 3);
+		glBindVertexArray(0);
+
+		glm::mat4 lowerLegL;
+		scale = glm::scale(lowerLegL, glm::vec3(0.3f, 0.7f, 0.3f));
+		translate = glm::translate(lowerLegL, glm::vec3(6.0f, -4.5f, 0.0f));
+		lowerLegL *= scale * translate;
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(lowerLegL));		
+
+		//TAIL
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 12 * 3);
+		glBindVertexArray(0);
+
+		glm::mat4 tail;
+		scale = glm::scale(tail, glm::vec3(2.0f, 0.3f, 0.3f));
+		translate = glm::translate(tail, glm::vec3(4.0f, 0.5f, 0.0f));
+		rotate = glm::rotate(tail, 45.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+		tail *= rotate * translate * scale;
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(tail));
+		
+		// Swap the screen buffers
+		glfwSwapBuffers(window);
 	}
 
+	// Terminate GLFW, clearing any resources allocated by GLFW.
+	glfwTerminate();
+	return 0;
 }
-
--- a1_27890834/template/vertex.shader
+++ a1_27890834/template/vertex.shader
@@ -1,11 +1,15 @@
-
 #version 330 core
-  
-layout (location = 0) in vec3 position;
 
+layout(location = 0) in vec3 position;
 
-out vec3 col;
+out vec4 col;
+
+uniform mat4 model_matrix;
+uniform mat4 view_matrix;
+uniform mat4 projection_matrix;
+
 void main()
 {
-    gl_Position =  vec4(position, 1.0);
+	gl_Position = projection_matrix * view_matrix * model_matrix * vec4(position.x, position.y, position.z, 1.0f);
+	col = vec4(position, 1.0f);
 }
